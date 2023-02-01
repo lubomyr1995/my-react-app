f55ae595d39a0adb622878ba5a04908688ab49eb
lubomyr <lubomyr1995@gmail.com>
2023-02-01 19:28:29 +0200

    add pagination

--- src/pages/ToDosPage/ToDosPage.js
+++ src/pages/ToDosPage/ToDosPage.js
@@ -1,17 +1,33 @@
 import {useEffect, useState} from "react";
 import {todosService} from "../../services";
 import {ToDo} from "../../components";
+import {useSearchParams} from "react-router-dom";
 
 const ToDosPage = () => {
+    const limit = 7
     const [toDos, setTodos] = useState([]);
 
+    const [query, setQuery] = useSearchParams({page: 1});
+
     useEffect(() => {
-        todosService.getAll().then(({data}) => setTodos([...data]))
-    }, [])
+        todosService.getAll(query.get('page'), limit).then(({data}) => setTodos([...data]))
+    }, [query])
+
+    let queryObj = Object.fromEntries(query.entries())
+    const nextPage = () => {
+        queryObj.page++
+        setQuery(queryObj)
+    }
+    const prevPage = () => {
+        queryObj.page--
+        setQuery(queryObj)
+    }
 
     return (
         <div>
             {toDos.map(todo => <ToDo key={todo.id} todo={todo}/>)}
+            <button disabled={queryObj.page <= 1} onClick={prevPage}>prev</button>
+            <button disabled={queryObj.page > Math.ceil(200 / limit) -1} onClick={nextPage}>next</button>
         </div>
     );
 };
--- src/services/todos.service.js
+++ src/services/todos.service.js
@@ -2,7 +2,12 @@ import {axiosService} from "./axios.service";
 import {urls} from "../constants";
 
 const todosService = {
-    getAll: () => axiosService.get(urls.todos)
+    getAll: (page, limit) => axiosService.get(urls.todos, {
+        params: {
+            _start: (page-1)*limit,
+            _limit: limit
+        }
+    })
 }
 
 export {todosService}
